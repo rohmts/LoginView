831949fabc7b2be6f884367e097a69752483d42c
Rohmats <rohmatsuseno@gmail.com>
2018-08-23 14:28:27 +0700

    Add UITextField & UIButton

--- LoginView/LoginController.swift
+++ LoginView/LoginController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    var loginView: LoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    func setupView() {
+        let mainView = LoginView(frame: self.view.frame)
+        self.loginView = mainView
+        self.loginView.loginAction = loginPressed
+        self.loginView.signupAction = signupPressed
+        self.view.addSubview(loginView)
+        loginView.setAnchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    }
+    
+    func loginPressed() {
+        print("login pressed")
+    }
+    
+    func signupPressed() {
+        print("signup pressed")
     }
 }
 
