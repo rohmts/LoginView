2bed5053543acda9af4d3a7a1ef3b96521765073
Rohmats <rohmatsuseno@gmail.com>
2018-08-23 11:38:45 +0700

    initial commit

---
+++ LoginView/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by Rohmat Suseno on 23/08/18.
+//  Copyright © 2018 Innovation Bdg. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
